9495885147b399989418c2d5c73cf48cdbf7b9d5
asketsystem <asketsystem1@gmail.com>
2022-02-12 10:36:54 +0000

    Refactoring to Multiple Plugins

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,35 +6,11 @@ const fileCache = localForage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = (inputCode: string) => {
+export const fetchPlugin = (inputCode: string) => {
   return {
-    name: 'unpkg-path-plugin',
+    name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      // Handle root entry file of 'index.js'
-      build.onResolve({ filter: /(^index\.js$)/ }, () => {
-        return { path: 'index.js', namespace: 'a' };
-      });
-
-      // Handle relative paths in a module
-      build.onResolve({ filter: /^\.+\// }, (args: any) => {
-        return {
-          namespace: 'a',
-          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
-            .href,
-        };
-      });
-
-      // Handle main file of a module
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        return {
-          namespace: 'a',
-          path: `https://unpkg.com/${args.path}`,
-        };
-      });
-
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
